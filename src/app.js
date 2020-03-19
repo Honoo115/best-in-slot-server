0e5f21376de1b2dc4c0bb459075b394d4719ff29
unknown <ixentrik@gmail.com>
2020-03-18 18:19:00 -0700

    Fixed remote database

--- src/app.js
+++ src/app.js
@@ -23,8 +23,8 @@ app.use(slotsRouter)
 app.use(function errorHandler(error, req, res, next) {
     let response;
     if (NODE_ENV === "production") {
-        // response = { error: { message: "server error" } };
-        response = { message: error.message, error };
+        response = { error: { message: "server error" } };
+        // response = { message: error.message, error };
     } else {
         response = { message: error.message, error };
     }
